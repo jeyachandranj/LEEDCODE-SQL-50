02c5c24d96b8d9e982418e83d9e2806d430f9582
jeyachandranj <125851197+jeyachandranj@users.noreply.github.com>
2024-05-12 23:33:34 +0530

    Update 6.Replace Employee ID With The Unique Identifier.sql

--- 6.Replace Employee ID With The Unique Identifier.sql
+++ 6.Replace Employee ID With The Unique Identifier.sql
@@ -1,1 +1,34 @@
+-- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+-- Return the result table in any order.
+-- The result format is in the following example.
+-- Example 1:
+-- Input: 
+-- Employees table:
+-- +----+----------+
+-- | id | name     |
+-- +----+----------+
+-- | 1  | Alice    |
+-- | 7  | Bob      |
+-- | 11 | Meir     |
+-- | 90 | Winston  |
+-- | 3  | Jonathan |
+-- +----+----------+
+-- EmployeeUNI table:
+-- +----+-----------+
+-- | id | unique_id |
+-- +----+-----------+
+-- | 3  | 1         |
+-- | 11 | 2         |
+-- | 90 | 3         |
+-- +----+-----------+
+-- Output: 
+-- +-----------+----------+
+-- | unique_id | name     |
+-- +-----------+----------+
+-- | null      | Alice    |
+-- | null      | Bob      |
+-- | 2         | Meir     |
+-- | 3         | Winston  |
+-- | 1         | Jonathan |
+-- +-----------+----------+
 select e2.unique_id,e1.name from Employees e1 left join EmployeeUNI e2 on e1.id = e2.id
